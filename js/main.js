903d81c347efd7ea1e6b0376be1b67cee35fb7b6
rafaelrochamor <ra_faelrocha@hotmail.com>
2023-08-21 13:40:16 -0300

    Terceiro Commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let offsetX, offsetY, dragging = false;
 
+    
+    const zoomInButton = document.querySelector('.zoom-in');
+    const zoomOutButton = document.querySelector('.zoom-out');
+    const flowCard = document.querySelector('.flow-card-start');
+    
+    let currentScale = 1; // Valor inicial da escala
+    const maxZooms = 5; // Máximo de zooms permitidos
+    
+    zoomInButton.addEventListener('click', () => {
+      if (currentScale < 1.5) { // Verifica se a escala não ultrapassa o limite
+        currentScale += 0.1; // Incrementa o valor da escala
+        flowCard.style.transform = `scale(${currentScale})`; // Aplica a nova escala
+      }
+    });
+    
+    zoomOutButton.addEventListener('click', () => {
+      if (currentScale > 0.6) { // Verifica se a escala não ultrapassa o limite
+        currentScale -= 0.1; // Decrementa o valor da escala
+        flowCard.style.transform = `scale(${currentScale})`; // Aplica a nova escala
+      }
+    });
+    
+    
+
     inicioFluxo.addEventListener('mousedown', e => {
         if (e.button !== 0) return;
     
@@ -43,6 +67,8 @@ document.addEventListener('DOMContentLoaded', () => {
         alert(`Publicando fluxo com título: ${title}`);
     });
 
+    
+    
 
 
 });
